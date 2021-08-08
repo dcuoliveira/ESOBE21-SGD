a1f2f0d0d8264a23f0ed9da1e95fd2768a2efc71
dcuoliveira <dan.cunhaa@gmail.com>
2021-08-08 19:49:19 -0300

    add parameters and plots

--- src/run_SGD_coint_test.R
+++ src/run_SGD_coint_test.R
@@ -18,16 +18,38 @@ bnlr = stan_glm(SGD ~ KRW + MYR + CNY + THB + IDR + TWD +
 Yt = bnlr$data$SGD
 Xt = bnlr$data %>% select(KRW, MYR, CNY, THB, IDR, TWD, INR, JPY, EUR, AUD, GBP) %>% t()
 Rt = bnlr$residuals
-t = dim(Xt)[2]
-k = dim(Xt)[1]
+T = dim(Xt)[2]
+n = dim(Xt)[1]
+k = 2
 npost = 5000
 
 sample_list_out = ARp_resid_coint_test_gibbs_sampler(Yt=Yt,
                                                      Xt=Xt,
                                                      Rt=Rt,
                                                      npost=npost,
-                                                     k=k,
-                                                     t=t)
+                                                     n=n,
+                                                     T=T,
+                                                     k=k)
 
+beta_list=sample_list_out$beta_list
+rho_xi_list=sample_list_out$rho_xi_list
+sigma_list=sample_list_out$sigma_list
 
+ts.plot(Rt, main = "Model residuals")
+
+boxplot(sqrt(unlist(sigma_list))[1001:npost], main = "Sigma")
+
+xi <- numeric(0)
+for(h in 1:npost){
+  xi[h] <- rho_xi_list[[h]][2]
+}
+boxplot(xi[1001:npost], main = "Xi")
+quantile(xi[1001:npost], c(0.025,0.975))
+
+rho <- numeric(0)
+for(h in 1:npost){
+  rho[h] <- rho_xi_list[[h]][1]
+}
+boxplot(rho[1001:npost], main = "Rho")
+quantile(rho[1001:npost], c(0.005,0.995))
 
